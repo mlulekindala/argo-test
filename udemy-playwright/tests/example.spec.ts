14bcf26b7e72532e26860effe6f0b2451c5cc221
Mluleki <Mluleki-Phemelo.Ndala@absa.africa>
2022-09-14 20:52:18 +0200

    Adding failing test

--- udemy-playwright/tests/example.spec.ts
+++ udemy-playwright/tests/example.spec.ts
@@ -1,7 +1,13 @@
 import { test, expect } from '@playwright/test'
 
-test.only('Simple basic test', async ({ page }) => {
+test('Simple basic test', async ({ page }) => {
   await page.goto('https://www.example.com')
   const pageTitle = await page.locator('h1')
   await expect(pageTitle).toContainText('Example Domain')
+})
+
+test('Simple basic test that fails @fails', async ({ page }) => {
+  await page.goto('https://www.example.com')
+  const pageTitle = await page.locator('h1')
+  await expect(pageTitle).toContainText('Fail Example')
 })
